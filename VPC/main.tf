2fd02d429772c16c561fa996f2b09778a3a202b4
Repliika <liddergg@gmail.com>
2022-01-05 08:36:11 -0500

    variables abstracted from main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -2,31 +2,32 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name   = "${local.vpc_name}-VPC"
-  cidr   = "10.0.0.0/16"
+  cidr   = var.cidr
 
 
 
   #SUBNET DECLARATIONS
-  azs              = ["eu-east-1a", "eu-east-1b"]
-  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
-  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  database_subnets = ["10.0.151.0/24", "10.0.152.0/24"]
+  azs              = var.az
+  public_subnets   = var.public_subnets
+  private_subnets  = var.private_subnets
+  database_subnets = var.database_subnets
 
 
   #DATABASE SUBNETS NO CONNECTIONS
-  create_database_subnet_group       = true
-  create_database_subnet_route_table = true
-
+  create_database_subnet_group       = var.create_database_subnet_group
+  create_database_subnet_route_table = var.create_database_subnet_route_table
+  #create_database_internet_gateway_route = true
+  #create_database_nat_gateway_route = true
 
   #CREATE + ENABLE NAT, DNS
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+  single_nat_gateway = var.vpc_single_nat_gateway
+  #enable_nat_gateway = true
+  #single_nat_gatway = true
+
+
   enable_dns_hostnames = true
   enable_dns_support   = true
-
-  #Local variables to make name and tags
-
-
 
   #TAGS
   private_subnet_tags = {
--- VPC/variables.tf
+++ VPC/variables.tf
@@ -6,20 +6,82 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-#Environment
+variable "cidr" {
+  description = "cidr bloc"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+#Environment name
 variable "env" {
   description = "environment used as a prefix"
   type        = string
   default     = "DEV"
 }
 
-#Department
+#Department name
 variable "department" {
   description = "department name as a prefix"
   type        = string
   default     = "HR"
 }
 
+#Availability zone 
+variable "az" {
+  type    = list(string)
+  default = ["eu-east-1a", "eu-east-1b"]
+}
+
+#Public subnet CIDR
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.102.0/24", "10.0.103.0/24"]
+}
+
+#Private subnet CIDR
+variable "private_subnets" {
+  description = "CIDR for private subnets"
+  type        = list(string)
+  default     = ["10.0.104.0/24", "10.0.105.0/24"]
+}
+
+#Database subnet CIDR
+variable "database_subnets" {
+  description = "CIDR for DB subnets"
+  type        = list(string)
+  default     = ["10.0.110.0/24", "10.0.102.111/24"]
+}
+
+# VPC create DB subent(True/False)
+variable "vpc_create_database_subnet_group" {
+  description = "VPC create DB Subnet group"
+  type        = bool
+  default     = true
+}
+
+# VPC create DB route table (True/False)
+variable "vpc_create_database_subnet_route_table" {
+  description = "VPC create DB route table for DB subnet"
+  type        = bool
+  default     = true
+}
+
+
+# VPC Enable NAT Gateway (True/False) 
+variable "vpc_enable_nat_gateway" {
+  description = "enable NAT Gateways for egress communication"
+  type        = bool
+  default     = true
+}
+
+# VPC single NAT Gateway per AZ (True/False)
+variable "vpc_single_nat_gateway" {
+  description = "one NAT Gateway per AZ"
+  type        = bool
+  default     = true
+}
+
+#Local variable to function the name and tags
 locals {
   department = var.department
   env        = var.env
