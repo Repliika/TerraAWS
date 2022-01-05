1df99917027d9edcd09e6820547dc1bc6aaf65ab
Repliika <liddergg@gmail.com>
2022-01-05 06:24:07 -0500

    added locals and variabled the tags

--- VPC/providers.tf
+++ VPC/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.70.0"
+      version = ">= 3.70.0"
     }
   }
 }
@@ -10,5 +10,5 @@ terraform {
 provider "aws" {
   # Configuration options
   region  = var.aws_region
-  profile = "terraform"
+  profile = "ginoo"
 }
--- VPC/variables.tf
+++ VPC/variables.tf
@@ -1,5 +1,32 @@
-variable "aws_region" {
-  default = "us-east-1"
 
+#Region
+variable "aws_region" {
+  description = "region to create"
+  type        = string
+  default     = "us-east-1"
 }
 
+#Environment
+variable "env" {
+  description = "environment used as a prefix"
+  type        = string
+  default     = "DEV"
+}
+
+#Department
+variable "department" {
+  description = "department name as a prefix"
+  type        = string
+  default     = "HR"
+}
+
+locals {
+  department = var.department
+  env        = var.env
+  vpc_name   = "${var.department}-${var.env}"
+  common_tags = {
+    department  = local.department
+    environment = local.env
+    vpc         = local.vpc_name
+  }
+}
