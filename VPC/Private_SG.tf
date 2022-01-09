55ad9082fd3e8c2a50bff052504d0c73dd83b28b
Repliika <liddergg@gmail.com>
2022-01-09 04:54:40 -0500

    added private SG and SG outputs

--- VPC/Private_SG.tf
+++ VPC/Private_SG.tf
@@ -1,18 +1,18 @@
 #AWS EC2 Security Group Terraform Module for Public Bastion
-module "public_bastion_sg" {
+module "private_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "3.18.0"
 
   #name, description, vpc_id required
-  name        = "public-bastion-sg"
-  description = "Bastion with SSH and HTTP port open, egress ports are open"
+  name        = "private_sg"
+  description = "SG, ingress allow HTTP and SSH from VPC, egress open"
   vpc_id      = module.vpc.vpc_id
 
   #Ingress Rules with CIDR Block allowed (everyone)  
-  ingress_rules = ["ssh-tcp"]
+  ingress_rules = ["ssh-tcp", "http-80-tcp"]
 
   #Needs a block otherwise none used
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
 
   #Egress Rule - all open
   egress_rules = ["all-all"]
